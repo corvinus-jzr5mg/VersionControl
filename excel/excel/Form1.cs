044c9c15e858045ed78045f89dc89e223a617f9a
András Bognár <andras.bognar@stud.uni-corvinus.hu>
2021-10-08 13:50:58 +0200

    iratas

--- excel/excel/Form1.cs
+++ excel/excel/Form1.cs
@@ -12,49 +12,70 @@ using System.Reflection;
 
 namespace excel
 {
-    public partial class Form1 : Form
-    {
-        RealEstateEntities context = new RealEstateEntities();
-        List<Flat> Flats;
-        Excel.Application xlApp; // A Microsoft Excel alkalmazás
-        Excel.Workbook xlWB; // A létrehozott munkafüzet
-        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+	public partial class Form1 : Form
+	{
+		RealEstateEntities context = new RealEstateEntities();
+		List<Flat> Flats;
+		Excel.Application xlApp; // A Microsoft Excel alkalmazás
+		Excel.Workbook xlWB; // A létrehozott munkafüzet
+		Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
 
-        public Form1()
-        {
-            InitializeComponent();
-            LoadData();
-            CreateExcel();
-        }
+		public Form1()
+		{
+			InitializeComponent();
+			LoadData();
+			CreateExcel();
+		}
 
-        private void LoadData()
-        {
-            Flats = context.Flats.ToList();
-        }
+		private void LoadData()
+		{
+			Flats = context.Flats.ToList();
+		}
 
-        private void CreateExcel()
-        {
-            try
+		private void CreateExcel()
+		{
+			try
+			{
+				xlApp = new Excel.Application();
+
+				xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+				xlSheet = xlWB.ActiveSheet;
+
+				CreateTable();
+				xlApp.Visible = true;
+				xlApp.UserControl = true;
+
+			}
+			catch (Exception ex)
+			{
+				string errMsg = string.Format("Error : {0}\nLine: {1}", ex.Message, ex.Source);
+				MessageBox.Show(errMsg, "Error");
+
+				xlWB.Close(false, Type.Missing, Type.Missing);
+				xlApp.Quit();
+				xlWB = null;
+				xlApp = null;
+			}
+		}
+
+		private void CreateTable()
+		{
+			string[] headers = new string[] {
+		 "Kód",
+		"Eladó",
+	 "Oldal",
+	 "Kerület",
+	 "Lift",
+	 "Szobák száma",
+	 "Alapterület (m2)",
+	 "Ár (mFt)",
+	 "Négyzetméter ár (Ft/m2)"};
+            for (int i = 0; i < headers.Length; i++)
             {
-                xlApp = new Excel.Application();
-
-                xlWB = xlApp.Workbooks.Add(Missing.Value);
-
-                xlSheet = xlWB.ActiveSheet;
-
-                xlApp.Visible = true;
-                xlApp.UserControl = true;
+				xlSheet.Cells[1,i+1] = headers[i];
             }
-            catch (Exception ex)
-            {
-                string errMsg = string.Format("Error : {0}\nLine: {1}", ex.Message, ex.Source);
-                MessageBox.Show(errMsg, "Error");
 
-                xlWB.Close(false, Type.Missing, Type.Missing);
-                xlApp.Quit();
-                xlWB = null;
-                xlApp = null;
-            }
-        }
-    }
+		}
+	}
 }
