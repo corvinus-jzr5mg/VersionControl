37259da8d8a5bd7b0defe0af83360fcdec1c3773
András Bognár <andras.bognar@stud.uni-corvinus.hu>
2021-10-23 12:30:22 +0200

    prtfolio itemek

--- VaR_mutato_jzr5mg/VaR_mutato_jzr5mg/Form1.cs
+++ VaR_mutato_jzr5mg/VaR_mutato_jzr5mg/Form1.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VaR_mutato_jzr5mg.Entities;
 
 namespace VaR_mutato_jzr5mg
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
+
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
