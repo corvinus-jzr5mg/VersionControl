f9ef5a5eaacd33310599eec849bb2ba0610815ce
András Bognár <andras.bognar@stud.uni-corvinus.hu>
2021-11-23 12:10:58 +0100

    jajj elfeljtek commitolni mindig

---
+++ mikulas/mikulas/Entities/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mikulas.Entities
+{
+    public class Ball : Label
+    {
+        public Ball()
+        {
+            this.AutoSize = false;
+            this.Height = this.Width = 50;
+            Paint += Ball_Paint;
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        public void MoveBall()
+        {
+            this.Left += 1;
+        }
+    }
+    
+}
