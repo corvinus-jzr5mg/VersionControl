e0f10fc0dec2a53a418c0dec204a12aef2434b89
András Bognár <andras.bognar@stud.uni-corvinus.hu>
2021-11-23 13:20:43 +0100

    commitka

---
+++ mikulas/mikulas/Abstractions/Toy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mikulas.Abstractions
+{
+    public abstract class Toy : Label
+    {
+        public Toy()
+        {
+            this.AutoSize = false;
+            this.Height = this.Width = 50;
+            Paint += Toy_Paint;
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected abstract void DrawImage(Graphics g);
+
+        public virtual void MoveBall()
+        {
+            this.Left += 1;
+        }
+    }
+}
