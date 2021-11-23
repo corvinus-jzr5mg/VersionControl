0492e397c00b288c8675ee36f93514eb05e28385
András Bognár <andras.bognar@stud.uni-corvinus.hu>
2021-11-23 14:11:33 +0100

    color picker

--- mikulas/mikulas/Abstractions/Toy.cs
+++ mikulas/mikulas/Abstractions/Toy.cs
@@ -24,7 +24,7 @@ namespace mikulas.Abstractions
 
         protected abstract void DrawImage(Graphics g);
 
-        public virtual void MoveBall()
+        public virtual void MoveToy()
         {
             this.Left += 1;
         }
--- mikulas/mikulas/Form1.Designer.cs
+++ mikulas/mikulas/Form1.Designer.cs
@@ -33,6 +33,10 @@ namespace mikulas
             this.mainPanel = new System.Windows.Forms.Panel();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.btnSelectCar = new System.Windows.Forms.Button();
+            this.btnSelectBall = new System.Windows.Forms.Button();
+            this.lblNext = new System.Windows.Forms.Label();
+            this.btnColor = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -54,15 +58,60 @@ namespace mikulas
             this.conveyorTimer.Interval = 10;
             this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
             // 
+            // btnSelectCar
+            // 
+            this.btnSelectCar.Location = new System.Drawing.Point(13, 13);
+            this.btnSelectCar.Name = "btnSelectCar";
+            this.btnSelectCar.Size = new System.Drawing.Size(181, 31);
+            this.btnSelectCar.TabIndex = 1;
+            this.btnSelectCar.Text = "Car";
+            this.btnSelectCar.UseVisualStyleBackColor = true;
+            this.btnSelectCar.Click += new System.EventHandler(this.btnSelectCar_Click);
+            // 
+            // btnSelectBall
+            // 
+            this.btnSelectBall.Location = new System.Drawing.Point(200, 13);
+            this.btnSelectBall.Name = "btnSelectBall";
+            this.btnSelectBall.Size = new System.Drawing.Size(181, 31);
+            this.btnSelectBall.TabIndex = 1;
+            this.btnSelectBall.Text = "Ball";
+            this.btnSelectBall.UseVisualStyleBackColor = true;
+            this.btnSelectBall.Click += new System.EventHandler(this.btnSelectBall_Click);
+            // 
+            // lblNext
+            // 
+            this.lblNext.AutoSize = true;
+            this.lblNext.Location = new System.Drawing.Point(411, 22);
+            this.lblNext.Name = "lblNext";
+            this.lblNext.Size = new System.Drawing.Size(68, 13);
+            this.lblNext.TabIndex = 2;
+            this.lblNext.Text = "Coming next:";
+            this.lblNext.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnColor.Location = new System.Drawing.Point(640, 17);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(91, 23);
+            this.btnColor.TabIndex = 3;
+            this.btnColor.UseVisualStyleBackColor = false;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1075, 482);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.lblNext);
+            this.Controls.Add(this.btnSelectBall);
+            this.Controls.Add(this.btnSelectCar);
             this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,6 +120,10 @@ namespace mikulas
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Button btnSelectCar;
+        private System.Windows.Forms.Button btnSelectBall;
+        private System.Windows.Forms.Label lblNext;
+        private System.Windows.Forms.Button btnColor;
     }
 }
 
