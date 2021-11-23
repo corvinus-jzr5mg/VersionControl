300fde95f80f90d5c4165edd5a8f26c7c996ab2d
András Bognár <andras.bognar@stud.uni-corvinus.hu>
2021-11-23 13:43:27 +0100

    carfactory abs

--- mikulas/mikulas/Entities/Car.cs
+++ mikulas/mikulas/Entities/Car.cs
@@ -12,7 +12,8 @@ namespace mikulas.Entities
     {
         protected override void DrawImage(Graphics g)
         {
-            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
         }
     }
 }
